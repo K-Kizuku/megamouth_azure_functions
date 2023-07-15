2d9a4a6e4b56844746e7fc6966397fbcea19bc43
K-Kizuku <kotani.kizuku@gmail.com>
2023-07-16 05:21:42 +0900

    third commit

--- CreateSasToken/CreateSasToken.cs
+++ CreateSasToken/CreateSasToken.cs
@@ -1,47 +1,52 @@
 ﻿using System;
-using System.IO;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
-using Microsoft.Azure;
 
 namespace CreateSasToken
 {
     public static class CreateSasToken
     {
         [FunctionName("CreateSasToken")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
             string blobName = req.Query["name"];
             var accountKey = Environment.GetEnvironmentVariable("AccountKey");
             var contaunerName = Environment.GetEnvironmentVariable("BlobName");
+            try
+            {
+                var storageAccount = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable(accountKey));
+                var blobClient = storageAccount.CreateCloudBlobClient();
+                var container = blobClient.GetContainerReference(contaunerName);
+                container.CreateIfNotExistsAsync();
+                var blob = container.GetBlockBlobReference(blobName);
 
-            var storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting(accountKey));
+                var sasContraints = new SharedAccessBlobPolicy
+                {
+                    SharedAccessStartTime = DateTime.UtcNow.AddMinutes(-5),
+                    SharedAccessExpiryTime = DateTime.UtcNow.AddHours(24),
+                    Permissions = SharedAccessBlobPermissions.Read | SharedAccessBlobPermissions.Write
+                };
 
-            var blobClient = storageAccount.CreateCloudBlobClient();
+                var ipRange = new IPAddressOrRange("192.168.192.168");
 
-            var container = blobClient.GetContainerReference(contaunerName);
-            await container.CreateIfNotExistsAsync();
+                var sasBlobToken = blob.GetSharedAccessSignature(sasContraints, null, null, null, ipRange);
 
-            var blob = container.GetBlockBlobReference(blobName);
-
-            var sasContraints = new SharedAccessBlobPolicy();
-            sasContraints.SharedAccessStartTime = DateTime.UtcNow.AddMinutes(-5);
-            sasContraints.SharedAccessExpiryTime = DateTime.UtcNow.AddHours(24);
-            sasContraints.Permissions = SharedAccessBlobPermissions.Read | SharedAccessBlobPermissions.Write;
-
-            var sasBlobToken = blob.GetSharedAccessSignature(sasContraints, null, null, null, null);
-
-            return new OkObjectResult(sasBlobToken);
+                return new OkObjectResult(sasBlobToken);
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex, "An error occurred while generating the SAS token.");
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
         }
     }
 }
+
 
